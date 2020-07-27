068acbc5d05a496fa9a3feb654d33d9fe980fd36
root <root@nishishailesh.ml>
2020-07-27 23:58:42 +0530

    modified:   search_si.php for R and U

--- search_si.php
+++ search_si.php
@@ -2,7 +2,6 @@
 //$GLOBALS['nojunk']='';
 require_once 'project_common.php';
 require_once 'base/verify_login.php';
-//echo '<link rel="stylesheet" type="text/css" media="print" href="bootstrap/css/bootstrap.min.css">';
 	////////User code below/////////////////////
 echo '		  <link rel="stylesheet" href="project_common.css">
 		  <script src="project_common.js"></script>';
@@ -10,7 +9,7 @@ $link=get_link($GLOBALS['main_user'],$GLOBALS['main_pass']);
 
 main_menu();
 echo '<h3>Search and display editable sample_IDs</h3>';
-//echo '<pre>';print_r($_POST);
+//echo '<pre>';print_r($GLOBALS);
 //echo '</pre>';
 
 echo '<div id=response></div>';
@@ -56,7 +55,16 @@ elseif($_POST['action']=='search')
 		foreach ($temp as $sid)
 		{
 			//view_sample($link,$sid);
-			sample_id_edit_button($sid,'target=_blank',$sid);
+
+			$released_by_local=get_one_ex_result($link,$sid,$GLOBALS['released_by']);
+			if(strlen($released_by_local)==0)
+			{
+				sample_id_edit_button($sid,'target=_blank',$sid.'<span class="bg-warning rounded">U</span>');
+			}
+			else
+			{
+				sample_id_edit_button($sid,'target=_blank',$sid.'<span class="bg-danger rounded">R</span>');
+			}
 		}	
 	}
 	else
