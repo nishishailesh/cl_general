66f4d9234917a93e17ab4fedbc4b9367877daf54
root <biochemistrygmcs@gmail.com>
2021-02-24 09:10:29 +0530

    modified:   config.php 	modified:   monitor.php 	modified:   project_common.php

--- monitor.php
+++ monitor.php
@@ -5,7 +5,7 @@ require_once 'project_common.php';
 require_once $GLOBALS['main_user_location'];
 
 $link=get_link($GLOBALS['main_user'],$GLOBALS['main_pass']);
-$lot_size=100;
+$lot_size=200;
 
 echo '<h5 class="text-success">'.strftime("%Y-%m-%d %H:%M:%S").'</h5>';
 $one='select max(sample_id) as max_id from result where sample_id between 1000000 and 1999999';
@@ -13,7 +13,7 @@ $result=run_query($link,$GLOBALS['database'],$one);
 if($result)
 {
 	$ar=get_single_row($result);
-	echo '<span style="border-radius:60px" class="badge bg-warning">Biochemistry Last ID<br><h1>'.$ar['max_id'].'</h1></span>';
+	echo '<span style="border-radius:60px" class="badge bg-warning">Biochemistry Last ID<br><h5>'.$ar['max_id'].'</h5></span>';
 }
 echo '<div class="d-block">';
 //requested //collected //receipt //preprocessed // analysed //released
